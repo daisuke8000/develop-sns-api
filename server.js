04b7cd2751844e898bf6b575a6e3ef55dc83ee8a
daisuke8000 <daisuke12108000@gmail.com>
2023-07-01 15:24:22 +0900

    setting prisma

--- server.js
+++ server.js
@@ -1,7 +1,56 @@
 const express = require('express');
 const app = express();
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
 const PORT = 5050
+
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+// User SignUp
+app.post("/api/auth/signup",async  (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user= await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    return res.json({user});
+})
+
+// User LogIn
+app.post("/api/auth/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if(!user) {
+        return res.status(401).json({error: "Invalid email or password"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.status(401).json({error: "Invalid email or password"});
+    }
+
+    const token = jwt.sign({id: user.id}, process.env.SECRET_KEY, {
+        expiresIn: "1d",
+    });
+
+    return res.json({token});
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
